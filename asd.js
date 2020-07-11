3c2b6daa1fe140caeeaabc041a159ac58b02d5d5
xanderalex7 <xander80@msn.com>
2020-07-11 15:35:01 +0200

    Yes.

---
+++ asd.js
@@ -0,0 +1,5 @@
+const  chadify = require('./index');
+
+let asd = chadify(true, false, 1, 1231223123)
+
+console.log(asd)
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = function chadify (...args) {return args ? "Yes." : "Yes."}
