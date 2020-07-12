499edef81649b524c5b7612f15e29f64c9456058
xanderalex7 <xander80@msn.com>
2020-07-12 22:57:17 +0200

    Yes.

--- chadify.test.js
+++ chadify.test.js
@@ -7,5 +7,35 @@ describe("Chadify function", () => {
     const response = chadify("string");
 
     expect(response).toBe(expectedResult);
+
+    it("Should return Yes. if an integer is passed", () => {
+      const response = chadify(135);
+  
+      expect(response).toBe(expectedResult);
+    });
+  
+    it("Should return Yes. if more than one value is passed", () => {
+      const response = chadify("First value", "Second value");
+  
+      expect(response).toBe(expectedResult);
+    });
+  
+    it("Should return Yes. if true is passed", () => {
+      const response = chadify(true);
+  
+      expect(response).toBe(expectedResult);
+    });
+  
+    it("Should return Yes. if false is passed", () => {
+      const response = chadify(false);
+  
+      expect(response).toBe(expectedResult);
+    });
+  
+    it("Should return Yes. if no value is passed", () => {
+      const response = chadify();
+  
+      expect(response).toBe(expectedResult);
+    });
   });
 });
