c753abc18f568049b7e3c69fe139833113061217
xanderalex7 <xander80@msn.com>
2020-08-01 12:57:21 +0200

    Yes.

---
+++ chadCli.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer")
+const chalk = require("chalk")
+const figlet = require("figlet")
+
+
+const init = () => {
+    console.log(
+      chalk.green(
+        figlet.textSync("Chadify 2.0", {
+          horizontalLayout: "default",
+          verticalLayout: "default"
+        })
+      )
+    );
+  };
+  
+  const askQuestions = () => {
+    const questions = [
+      {
+        name: "QUESTION",
+        type: "input",
+        message: "Ask me anything.\n\n"
+      }
+    ];
+    return inquirer.prompt(questions)
+  };
+
+  const yes = () => {
+    console.log(
+      chalk.green.bgBlack.bold("\nYes.\n")
+    );
+  };
+
+  const kBro = () => {
+    console.log(
+      chalk.green.bgBlack.bold("\nK bro, have a nice day.\n")
+    );
+  };
+  
+//   const chad = async () => {
+//     init();
+
+//     const answers = await askQuestions()
+//     const { QUESTION } = answers 
+
+//     if(QUESTION === "") {
+//         setTimeout(() => {kBro()}, 1000)
+//     } else {setTimeout(() => {yes()}, 1000)}
+
+//   };
+
+  module.exports = async function chad () {
+    init();
+
+    const answers = await askQuestions()
+    const { QUESTION } = answers 
+
+    if(QUESTION === "") {
+        setTimeout(() => {kBro()}, 1000)
+    } else {setTimeout(() => {yes()}, 1000)}
+
+  };
+  
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+const chad = require ('./chadCli')
 module.exports = function chadify (...args) {return args ? "Yes." : "Yes."}
+
+chad();
